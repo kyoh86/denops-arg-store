4e35765f93080372036b400d6f4293bfb709a9c9
kyoh86 <me@kyoh86.dev>
2024-05-05 01:18:14 +0900

    add dispatcher helpers

--- mod.ts
+++ mod.ts
@@ -1,5 +1,8 @@
+import { Denops } from "https://deno.land/x/denops_core@v6.0.5/mod.ts";
 import {
+  ensure,
   is,
+  Predicate,
   PredicateType,
 } from "https://deno.land/x/unknownutil@v3.18.0/mod.ts";
 
@@ -29,6 +32,26 @@ export class ArgStore {
   }
 
   /**
+   * Make a dispatcher to set an arg value for a function (or cross all functions).
+   * @example
+   * ```ts
+   * const argStore = new ArgStore();
+   * denops.dispatcher = {
+   *   customSetFuncArg: argStore.dispatchSetFuncArg(),
+   * }
+   * ```
+   */
+  public setFuncArgDispatcher() {
+    return (uFunc: unknown, uArg: unknown, value: unknown) => {
+      this.setFuncArg(
+        ensure(uFunc, is.String),
+        ensure(uArg, is.String),
+        value,
+      );
+    };
+  }
+
+  /**
    * Patch args for a function (or cross all functions).
    * @param {string} func - Target function name or "_" to cross all functions.
    * @param {Record<string, unknown>} args - A record holding arg name and value pairs.
@@ -38,12 +61,76 @@ export class ArgStore {
     this.#store.set(func, { old, ...args });
   }
 
-  /** Patch args for everything. */
+  /**
+   * Make a dispatcher to patch args for a function (or cross all functions).
+   * @example
+   * ```ts
+   * const argStore = new ArgStore();
+   * denops.dispatcher = {
+   *   customPatchFuncArgs: argStore.dispatchPatchFuncArgs(),
+   * }
+   * ```
+   */
+  public patchFuncArgsDispatcher() {
+    return (uFunc: unknown, uArgs: unknown) => {
+      this.patchFuncArgs(
+        ensure(uFunc, is.String),
+        ensure(uArgs, isArgs),
+      );
+    };
+  }
+
+  /** Patch args for each functions. */
   public patchArgs(argSet: Record<string | CrossFunc, Args>) {
     for (const func in argSet) {
       const old = this.#store.get(func) || {};
       this.#store.set(func, { old, ...argSet[func] });
     }
+  }
+
+  /**
+   * Make a dispatcher to patch args for each functions.
+   * @example
+   * ```ts
+   * const argStore = new ArgStore();
+   * denops.dispatcher = {
+   *   customPatchArgs: argStore.dispatchPatchArgs(),
+   * }
+   * ```
+   */
+  public patchArgsDispatcher() {
+    return (uArgs: unknown) => {
+      this.patchArgs(ensure(uArgs, is.RecordOf(isArgs)));
+    };
+  }
+
+  private ensureArgs<T>(func: string, pred: Predicate<T>, uArgs: unknown): T {
+    return ensure(this.getArgs(func, ensure(uArgs, isArgs)), pred);
+  }
+
+  /**
+   * Make a dispatcher to dispatch a function with args.
+   * @example
+   * ```ts
+   * const argStore = new ArgStore();
+   * const isSomeArgs = is.ObjectOf({
+   *   foo: is.String,
+   * })
+   * denops.dispatcher = {
+   *   someFunc: argStore.dispatchFunction(denops, "someFunc", isSomeArgs, (denops, args) => {
+   *     // Do something with args
+   *   }),
+   * }
+   */
+  public makeDispatcher<T extends Args>(
+    denops: Denops,
+    functionName: string,
+    isArgs: Predicate<T>,
+    functionBody: (denops: Denops, args: T) => void | Promise<void>,
+  ) {
+    return (uArgs: unknown) => {
+      return functionBody(denops, this.ensureArgs(functionName, isArgs, uArgs));
+    };
   }
 
   /**
